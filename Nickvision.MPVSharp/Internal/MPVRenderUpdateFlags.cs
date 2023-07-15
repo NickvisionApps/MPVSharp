640144c2abcc2d9068c575db29e44357ab8f3c1b
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-16 02:11:57 +0300

    Add MPVRenderUpdateFlags

---
+++ Nickvision.MPVSharp/Internal/MPVRenderUpdateFlags.cs
@@ -0,0 +1,11 @@
+namespace Nickvision.MPVSharp.Internal;
+
+/// <summary>
+/// Flags returned by MPVRenderContext.Update()
+/// </summary>
+public enum MPVRenderUpdateFlags {
+    /// <summary>
+    /// A new video frame must be rendered. MPVRenderContext.Render() must be called.
+    /// </summary>
+    UpdateFrame = 1 << 0
+}
