3d2d22b85d1a88ad2b0d8240fa98499ce56312a7
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-16 03:41:05 +0300

    Add missing types from render_gl.h

---
+++ Nickvision.MPVSharp/Internal/MPVOpenGLDRMDrawSurfaceSize.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+namespace Nickvision.MPVSharp.Internal;
+
+/// <summary>
+/// For MPVRenderParamType.DRMDrawSurfaceSize.
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+public struct MPVOpenGLDRMDrawSurfaceSize
+{
+    /// <summary>
+    /// Width of the draw plane surface in pixels.
+    /// </summary>
+    public int Width;
+    /// <summary>
+    /// Height of the draw plane surface in pixels.
+    /// </summary>
+    public int Height;
+}
---
+++ Nickvision.MPVSharp/Internal/MPVOpenGLDRMParamsV2.cs
@@ -0,0 +1,34 @@
+using System.Runtime.InteropServices;
+
+namespace Nickvision.MPVSharp.Internal;
+
+/// <summary>
+/// For MPVRenderParamType.DRMDisplayV2.
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+public struct MPVOpenGLDRMParamsV2
+{
+    /// <summary>
+    /// DRM fd (int). Set to -1 if invalid.
+    /// </summary>
+    public int Fd;
+    /// <summary>
+    /// Currently used Crtc Id
+    /// </summary>
+    public int CrtcId;
+    /// <summary>
+    /// Currently used connector id
+    /// </summary>
+    public int ConnectorId;
+    /// <summary>
+    /// Pointer to a drmModeAtomicReq pointer that is being used for the renderloop.
+    /// This pointer should hold a pointer to the atomic request pointer
+    /// The atomic request pointer is usually changed at every renderloop.
+    /// </summary>
+    public nint AtomicRequestPtr;
+    /// <summary>
+    /// DRM render node. Used for VAAPI interop.
+    /// Set to -1 if invalid.
+    /// </summary>
+    public int RenderFd;
+}
