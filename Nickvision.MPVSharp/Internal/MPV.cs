d8ee888522536923fd7bf3d7f4295a64b827a19a
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-17 18:19:40 +0300

    Add general MPV functions

---
+++ Nickvision.MPVSharp/Internal/MPV.cs
@@ -0,0 +1,23 @@
+using System.Runtime.InteropServices;
+
+namespace Nickvision.MPVSharp.Internal;
+
+/// <summary>
+/// General MPV functions
+/// </summary>
+public static partial class MPV {
+    [LibraryImport("libmpv.so.2")]
+    private static partial ulong mpv_client_api_version();
+    [LibraryImport("libmpv.so.2")]
+    private static partial void mpv_free(nint data);
+    
+    /// <summary>
+    /// Return the API version the mpv source has been compiled with.
+    /// </summary>
+    public static ulong APIVersion => mpv_client_api_version();
+    /// <summary>
+    /// General function to deallocate memory returned by some of the API functions.
+    /// Call this only if it's explicitly documented as allowed.
+    /// </summary>
+    public static void Free(nint data) => mpv_free(data);
+}
