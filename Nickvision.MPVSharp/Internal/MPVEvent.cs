b1d43a69d402065d433707f50263dd6b35d713f5
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-17 18:24:16 +0300

    MPVEvent - ToNode() and comments

--- Nickvision.MPVSharp/Internal/MPVEvent.cs
+++ Nickvision.MPVSharp/Internal/MPVEvent.cs
@@ -10,6 +10,8 @@ public partial struct MPVEvent
 {
     [LibraryImport("libmpv.so.2", StringMarshalling = StringMarshalling.Utf8)]
     private static partial string mpv_event_name(MPVEventId id);
+    [LibraryImport("libmpv.so.2", StringMarshalling = StringMarshalling.Utf8)]
+    private static partial MPVError mpv_event_to_node(out MPVNode node, ref MPVEvent e);
 
     /// <summary>
     /// Event Id
@@ -24,18 +26,20 @@ public partial struct MPVEvent
     /// </summary>
     public ulong ReplyUserdata;
     /// <summary>
-    /// Event data
+    /// Pointer to event data
     /// </summary>
     private nint _data;
 
     /// <summary>
     /// Get string describing the event
     /// </summary>
-    public string Name => mpv_event_name(Id);
+    /// <returns>Event name</returns>
+    public string GetName() => mpv_event_name(Id);
 
     /// <summary>
     /// Get EventLogMessage of LogMessage event
     /// </summary>
+    /// <returns>MPVEventLogMessage or null if wrong event Id</returns>
     public MPVEventLogMessage? GetEventLogMessage()
     {
         if (Id != MPVEventId.LogMessage)
@@ -48,6 +52,7 @@ public partial struct MPVEvent
     /// <summary>
     /// Get MPVEventProperty of PropertyChange or GetPropertyReply event
     /// </summary>
+    /// <returns>MPVEventProperty or null if wrong event Id</returns>
     public MPVEventProperty? GetEventProperty()
     {
         if ((Id != MPVEventId.PropertyChange && Id != MPVEventId.GetPropertyReply) || Error < (int)MPVError.Success)
@@ -57,6 +62,10 @@ public partial struct MPVEvent
         return Marshal.PtrToStructure<MPVEventProperty>(_data);
     }
 
+    /// <summary>
+    /// Get MPVEventCommand of CommandReply event
+    /// </summary>
+    /// <returns>MPVEventCommand or null if wrong event Id</returns>
     public MPVEventCommand? GetCommandResult()
     {
         if (Id != MPVEventId.CommandReply)
@@ -66,6 +75,10 @@ public partial struct MPVEvent
         return Marshal.PtrToStructure<MPVEventCommand>(_data);
     }
 
+    /// <summary>
+    /// Get MPVEventStartFile of StartFile event
+    /// </summary>
+    /// <returns>MPVEventStartFile or null if wrong event Id</returns>
     public MPVEventStartFile? GetStartFile()
     {
         if (Id != MPVEventId.StartFile)
@@ -75,6 +88,10 @@ public partial struct MPVEvent
         return Marshal.PtrToStructure<MPVEventStartFile>(_data);
     }
 
+    /// <summary>
+    /// Get MPVEventEndFile of EndFile event
+    /// </summary>
+    /// <returns>MPVEventEndFile or null if wrong event Id</returns>
     public MPVEventEndFile? GetEndFile()
     {
         if (Id != MPVEventId.EndFile)
@@ -84,6 +101,10 @@ public partial struct MPVEvent
         return Marshal.PtrToStructure<MPVEventEndFile>(_data);
     }
 
+    /// <summary>
+    /// Get client message of ClientMessage event
+    /// </summary>
+    /// <returns>Array of string args or null if wrong event Id</returns>
     public string[]? GetClientMessage()
     {
         if (Id != MPVEventId.ClientMessage)
@@ -93,6 +114,10 @@ public partial struct MPVEvent
         return MPVEventClientMessage.GetStringArgs(Marshal.PtrToStructure<MPVEventClientMessage>(_data));
     }
 
+    /// <summary>
+    /// Get MPVEventHook of Hook event
+    /// </summary>
+    /// <returns>MPVEventHook or null if wrong event Id</returns>
     public MPVEventHook? GetHook()
     {
         if (Id != MPVEventId.Hook)
@@ -101,4 +126,12 @@ public partial struct MPVEvent
         }
         return Marshal.PtrToStructure<MPVEventHook>(_data);
     }
+
+    /// <summary>
+    /// Convert given MPVEvent to MPVNode
+    /// </summary>
+    /// <param name="node">A node where to write result to</param>
+    /// <param name="e">Event</param>
+    /// <returns>Error code</returns>
+    public static MPVError ToNode(out MPVNode node, MPVEvent e) => mpv_event_to_node(out node, ref e);
 }
