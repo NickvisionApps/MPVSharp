86b5c1e44f01e1cf5449709c47c79c1fc53c62ae
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-03 01:02:05 +0300

    Update examples

--- Examples/GirCore/MainWindow.cs
+++ Examples/GirCore/MainWindow.cs
@@ -1,4 +1,5 @@
 using Nickvision.MPVSharp;
+using System;
 
 namespace Nickvision.MPVSharp.Examples.GirCore;
 
--- Examples/OpenGL/MPVWindow.cs
+++ Examples/OpenGL/MPVWindow.cs
@@ -3,6 +3,7 @@ using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
 
 namespace Nickvision.MPVSharp.Examples.OpenGL;
 
