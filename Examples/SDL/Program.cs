d096d5171399671aa92f11064e9f5f31f7fc9e49
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-03 18:07:47 +0300

    Simplify SDL example

--- Examples/SDL/Program.cs
+++ Examples/SDL/Program.cs
@@ -7,7 +7,6 @@ namespace Nickvision.MPVSharp.Examples.SDL;
 public class Program
 {
     private readonly nint _window;
-    private readonly nint _renderer;
     private readonly nint _glCtx;
     private readonly Client _player;
     private readonly RenderContext _playerCtx;
@@ -24,17 +23,14 @@ public class Program
         {
             throw new Exception($"Failed to initialize SDL: {SDL_GetError()}");
         }
-        _window = SDL_CreateWindow("SDL2 MPV Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL_WindowFlags.SDL_WINDOW_RESIZABLE | SDL_WindowFlags.SDL_WINDOW_OPENGL);
+        _window = SDL_CreateWindow("SDL2 MPV Example",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360,
+            SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL_WindowFlags.SDL_WINDOW_RESIZABLE | SDL_WindowFlags.SDL_WINDOW_OPENGL);
         if (_window == IntPtr.Zero)
         {
             throw new Exception($"Failed to create SDL Window: {SDL_GetError()}");
         }
         _glCtx = SDL_GL_CreateContext(_window);
-        _renderer = SDL_CreateRenderer(_window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
-        if (_renderer == IntPtr.Zero)
-        {
-            throw new Exception($"Failed to create renderer: {SDL_GetError()}");
-        }
         // Create MPV Client
         _player = new Client();
         _player.SetProperty("ytdl", true);
@@ -59,15 +55,19 @@ public class Program
         while (_running)
         {
             PollEvents();
-            Render();
+            // Render frame
+            SDL_GetWindowSize(_window, out var width, out var height);
+            _playerCtx.RenderGL(width, height);
+            SDL_GL_SwapWindow(_window);
         }
+        // Finalize
         _playerCtx.Dispose();
         _player.Dispose();
-        SDL_DestroyRenderer(_renderer);
         SDL_GL_DeleteContext(_glCtx);
         SDL_DestroyWindow(_window);
         SDL_Quit();
     }
+    
     /// <summary>
     /// Process SDL events
     /// </summary>
@@ -96,16 +96,5 @@ public class Program
                     break;
             }
         }
-    }
-    
-    /// <summary>
-    /// Render frame
-    /// </summary>
-    private void Render()
-    {
-        SDL_RenderClear(_renderer);
-        SDL_GetWindowSize(_window, out var width, out var height);
-        _playerCtx.RenderGL(width, height);
-        SDL_GL_SwapWindow(_window);
     }
 }
